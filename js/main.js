e1ab0ea49ca0affd0b1666ba7f22012be3cf12db
tannerkrewson <tannerkrewson@gmail.com>
2017-06-01 13:48:26 -0400

    Made the hidden elements take up no space

--- js/main.js
+++ js/main.js
@@ -2,13 +2,21 @@ $(".title").typed({
 	strings: ["Hi, I'm Tanner."],
 	typeSpeed: 100,
 	callback: function() {
-		showSubtitle();
+		// done once the typing animation is complete
+
+		// make the elements take up space
+		// by removing display: none
+		$(".subtitle").removeClass("non-existant");
+		$(".content").removeClass("non-existant");
+
+		// if it has less than an 11ms delay, the fade
+		// animation won't play
+		setTimeout(showSubtitle, 11);
 	}
 });
 
 function showSubtitle(){
-	//show the rest of the page,
-	//once the typing animation finishes
+	// show the rest of the page
   $(".subtitle").addClass("fade");
 	$(".content").addClass("fade");
 }
