2acec60b91534c2a384aaf4ec575b045a0eb26dc
tannerkrewson <tannerkrewson@gmail.com>
2017-07-17 16:28:47 -0400

    Only play the typing animation if the page hasn't been visited in 16 hours

--- js/main.js
+++ js/main.js
@@ -1,26 +1,40 @@
+var TITLE_TEXT = "Hi, I'm Tanner.";
+
 $('#footer-year').text((new Date()).getFullYear());
 
-$(".title").typed({
-	strings: ["Hi, I'm Tanner."],
-	typeSpeed: 100,
-	callback: function() {
-		// done once the typing animation is complete
+var shouldPlayAnimation = !Cookies.get('typed');
+if (shouldPlayAnimation) {
+	$(".title").typed({
+		strings: [TITLE_TEXT],
+		typeSpeed: 100,
+		callback: showPage 
+	});
+} else {
+	var title = $(".title");
+	
+	// we don't fade the title when doing the typed
+	// animation... but we're not doing it anymore,
+	// so let's fade it
+	title.addClass("to-fade");
+	title.text(TITLE_TEXT);
+	showPage();
+}
 
-		// make the elements take up space
-		// by removing display: none
-		$(".subtitle").removeClass("non-existant");
-		$(".content").removeClass("non-existant");
+var sixteenHours = 16/24;
+Cookies.set('typed', 'true', { expires: sixteenHours });
 
-		// if it has less than an 11ms delay, the fade
-		// animation won't play
-		// 11ms would only work sometimes, so I made
-		// it wait longer
-		setTimeout(showSubtitle, 30);
-	}
-});
+function showPage() {
+	// make the elements take up space by removing
+	// display: none, without removing opacity 0
+	$(".non-existant").removeClass("non-existant");
 
-function showSubtitle(){
-	// show the rest of the page
-  $(".subtitle").addClass("fade");
-	$(".content").addClass("fade");
+	// if it has less than an 11ms delay, the fade
+	// animation won't play
+	// 11ms would only work sometimes, so I made
+	// it wait longer
+	setTimeout(function () {
+		var elemsToFade = $(".to-fade");
+		elemsToFade.removeClass("to-fade");
+		elemsToFade.addClass("fade");
+	}, 30);
 }
